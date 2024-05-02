cdf27452cd9cd9e9423695187b6557e6c90451e7
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-02 17:23:30 -0300

    Ajax e fetch para APIs

--- js/contato.js
+++ js/contato.js
@@ -11,7 +11,7 @@ const botaoBuscar = formulario.querySelector("#buscar");
 const mensagemStatus = formulario.querySelector("#status");
 
 /* Detectando quando o botão de buscar CEP é acionado */
-botaoBuscar.addEventListener("click", function(event){
+botaoBuscar.addEventListener("click", async function(event){
     /* Anular o comportamento padrão de
     redirecionamento/recarregamento da página. Sempre
     acontece ao trabalhar com <a> e <form>. */
@@ -38,11 +38,39 @@ botaoBuscar.addEventListener("click", function(event){
     (ou Web Services) */
 
     // Etapa 1: preparar a URL da API com o CEP informado
+    let url = `https://viacep.com.br/ws/${cepInformado}/json/`;
 
     // Etapa 2: acessar a API (com a URL) e aguardar o retorno dela
+    const resposta = await fetch(url);
 
     // Etapa 3: extrair os dados da resposta da API em formato JSON
+    const dados = await resposta.json();
 
     // Etapa 4: lidar com os dados (em caso de erro e de sucesso)
+    
+    /* Se existir a string/prop "erro" no objeto dados */
+    if( "erro" in dados ){
+        mensagemStatus.textContent = "CEP inexistente!";
+        mensagemStatus.style.color = "red";
+    } else {
+        /* Senão, é porque o CEP existe! */
+        mensagemStatus.textContent = "CEP encontrado!";
+        mensagemStatus.style.color = "blue";
+
+        /* Selecionando os elementos que estão escondidos */
+        const camposRestantes = formulario.querySelectorAll('.campos-restantes');
+
+        /* Removendo a classe usando um loop (isso fará os campos
+        aparecerem novamente) */
+        for(const campo of camposRestantes){
+            campo.classList.remove("campos-restantes");
+        }
+
+        /* Atribuindo os dados a cada campo */
+        campoEndereco.value = dados.logradouro;
+        campoBairro.value = dados.bairro;
+        campoCidade.value = dados.localidade;
+        campoEstado.value = dados.uf;
+    }
 
 });
