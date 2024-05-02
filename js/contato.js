98d3ec32c08e40174d1baaf746550b7b222e82c0
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-02 16:47:21 -0300

    Ações para captura do cep

--- js/contato.js
+++ js/contato.js
@@ -8,7 +8,7 @@ const campoBairro = formulario.querySelector("#bairro");
 const campoCidade = formulario.querySelector("#cidade");
 const campoEstado = formulario.querySelector("#estado");
 const botaoBuscar = formulario.querySelector("#buscar");
-const mensagem = formulario.querySelector("#status");
+const mensagemStatus = formulario.querySelector("#status");
 
 /* Detectando quando o botão de buscar CEP é acionado */
 botaoBuscar.addEventListener("click", function(event){
@@ -16,4 +16,33 @@ botaoBuscar.addEventListener("click", function(event){
     redirecionamento/recarregamento da página. Sempre
     acontece ao trabalhar com <a> e <form>. */
     event.preventDefault();
+
+    /* Verificando se o cep NÃO TEM 8 dígitos */
+    if( campoCep.value.length !== 8 ){
+        /* Informar o usuário sobre o erro: */
+        mensagemStatus.textContent = "Digite um CEP válido";
+        mensagemStatus.style.color = "purple";
+        
+        /* Parar completamente a execução */
+        return;
+    } 
+
+    /* Guardando o valor do cep digitado/informado */
+    let cepInformado = campoCep.value;
+    
+    /* AJAX - Asyncronous JavaScript And XML
+    (JavaScript assíncrono e XML)
+    
+    Técnica de comunicação (transmissão, recebimento) de dados
+    que permite o processamento em conjunto com APIs 
+    (ou Web Services) */
+
+    // Etapa 1: preparar a URL da API com o CEP informado
+
+    // Etapa 2: acessar a API (com a URL) e aguardar o retorno dela
+
+    // Etapa 3: extrair os dados da resposta da API em formato JSON
+
+    // Etapa 4: lidar com os dados (em caso de erro e de sucesso)
+
 });
