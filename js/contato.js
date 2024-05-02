c00c264a1afa548365e2baa4b1bf52989b26fec4
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-02 15:20:46 -0300

    JS inicial: seleção dos elementos

---
+++ js/contato.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* Selecionando os elementos que serão manipulados */
+const formulario = document.querySelector("form");
+const campoCep = formulario.querySelector("#cep");
+const campoEndereco = formulario.querySelector("#endereco");
+const campoBairro = formulario.querySelector("#bairro");
+const campoCidade = formulario.querySelector("#cidade");
+const campoEstado = formulario.querySelector("#estado");
+const botaoBuscar = formulario.querySelector("#buscar");
+const mensagem = formulario.querySelector("#status");
+
