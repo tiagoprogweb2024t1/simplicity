c9903fc155a92adca561462bdafe5335e788e824
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-05-09 17:04:20 -0300

    Formspree versão 2

--- js/contato.js
+++ js/contato.js
@@ -80,3 +80,35 @@ botaoBuscar.addEventListener("click", async function(event){
     }
 
 });
+
+
+/* Script do Formspree */
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status-do-envio");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: formulario.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your submission!";
+          formulario.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    formulario.addEventListener("submit", handleSubmit)
